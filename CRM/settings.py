2ac00ed7ad031a579423084b65c2dda422e3d896
sandeshBthapa <48406633+sandeshBthapa@users.noreply.github.com>
2020-07-14 21:19:28 +0545

    Update settings.py

--- CRM/settings.py
+++ CRM/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(k-coqd**y^+a3dx^mme8c%ydfa4y84y(+xa-woi2ag8b0g6pn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
